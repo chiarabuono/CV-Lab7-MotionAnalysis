1133c11ea7204d08161c1d9d272b4a85864c2873
Mattia <mattia.tinfena@gmail.com>
2024-12-25 12:04:28 +0100

    minor changes

--- main.m
+++ main.m
@@ -3,10 +3,10 @@ close all
 clc
 
 %% Part 1.1 - Comparison between static and moving average background
-%compareCDAlgo('videos/luce_vp.mp4', 30, 0.1, 25, 10);
+compareCDAlgo('videos/luce_vp.mp4', 30, 0.1, 25, 10);
 
 %% Part 1.2 - Comparison between moving average and optical flow 
-%compareCDOF('videos/tennis.mp4', 30, 0.2, 25, 10);
+compareCDOF('videos/tennis.mp4', 30, 0.2, 25, 10);
 
 %% Part 2 - Tracker of fixed target
 segmentAndTrack('videos/DibrisHall.mp4', 30, 0.2, 25);
